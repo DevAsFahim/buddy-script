06de77c5b2edaa92fd4a8099aa888873b49312c6
DevAsFahim <devasfahim@gmail.com>
2026-04-02 17:50:47 +0600

    working on error handler

---
+++ server/src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,25 @@
+import e, { ErrorRequestHandler } from "express";
+import { TErrorSources } from "../interface/error";
+import { ZodError } from "zod";
+import handleZodError from "../error/handleZodError";
+
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  // Default values
+  let statusCode = 500;
+  let message = "Something went wrong";
+
+  let errorSources: TErrorSources = [
+    {
+      path: "",
+      message: "Something went wrong",
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  }
+  
+};
