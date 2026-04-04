76e6e2f7fbc25db1ddba339f016087b3ac7ca7ac
DevAsFahim <devasfahim@gmail.com>
2026-04-04 15:22:13 +0600

    u

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import { userController } from "./app/modules/user/user.controller";
 import router from "./app/routes";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app: Application = express();
 
@@ -10,6 +11,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
 
 // routes
 app.use("/api/v1", router);
